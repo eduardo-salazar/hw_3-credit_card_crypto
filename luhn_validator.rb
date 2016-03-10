b410ed6cbc7e3cce73bfe8e1d72cd58520accadb
eduardo-salazar <esalazar922@gmail.com>
2016-03-10 17:26:32 +0800

    Fix code to rever array before validation

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -4,8 +4,16 @@ module LuhnValidator
   # assumes: a local String called 'number' exists
   # returns: true/false whether last digit is correct
   def validate_checksum
-    nums_a = number.to_s.chars.map(&:to_i)
 
-    # TODO: use the integers in nums_a to validate its last check digit
+    nums_a = number.to_s.chars.map(&:to_i)
+    sum = 0
+    nums_a.reverse.map.with_index do |d, i|
+    	if i.odd?
+    		sum += (d * 2).to_s.chars.map!(&:to_i).reduce(:+)
+    	else
+    		sum += d
+    	end
+    end
+    (sum % 10) == 0
   end
 end
